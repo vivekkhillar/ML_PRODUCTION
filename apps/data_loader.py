f26e0464f9b211616831a213c0203333c61bd1a6
Vivek Khillar <vivekkhillar@gmail.com>
2025-10-23 19:45:07 +0530

    added the data_loader.py

---
+++ apps/data_loader.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import os
+
+def data_load(path: str = "../data/customer_churn.csv") -> pd.DataFrame:
+    """
+    Load customer data from a CSV file.
+
+    Parameters:
+    path (str): The file path to the CSV file. Default is "../data/customer_data.csv".
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the customer data.
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"{path} not found")
+    
+    data = pd.read_csv(path)
+    print(data.head())
+    return data
+
+if __name__ == "__main__":
+    data_load()
