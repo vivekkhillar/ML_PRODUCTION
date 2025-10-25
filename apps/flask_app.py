02b61476101b10eb613a8edb2cf0a1638b62b011
Vivek Khillar <60787794+vivekkhillar@users.noreply.github.com>
2025-10-25 12:32:13 +0000

    flask_app code updated

--- apps/flask_app.py
+++ apps/flask_app.py
@@ -3,3 +3,30 @@ import joblib
 import pandas as pd
 import os
 
+app = flask(__name__)
+MODEL_DIR = "../model/model.pkl"
+
+
+if not os.path.exist(MODEL_PATH) :
+    raise FileNotFoundError("Model not found. Please run train.py first")
+
+
+model = joblib.load(MODEL_PATH)
+
+@app.route("/")
+def home():
+    return {"message": "Customer Churn Prediction API"}
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json(force=True)
+    df = pd.DataFrame([data])
+    pred = model.predict(df)[0]
+    proba = model.predict_proba(df)[0, 1]
+    return jsonify({"prediction": int(pred), "probability": float(proba)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
